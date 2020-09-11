7fdd264cd21d179f1b93423eadae039f324a6197
Elton Standerski <eltonstand@gmail.com>
2020-09-11 14:20:43 -0300

    Aulas 07 a 10

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,54 +1,111 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Curso {
 
-    private String nome;
-    private String instrutor;
-    private List<Aula> aulas = new LinkedList<Aula>();
-    private Set<Aluno> alunos = new HashSet<>();
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
-    public Curso(String nome, String instrutor) {
-        this.nome = nome;
-        this.instrutor = instrutor;
-    }
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getInstrutor() {
-        return instrutor;
-    }
+	public String getInstrutor() {
+		return instrutor;
+	}
 
-    public List<Aula> getAulas() {
-        return Collections.unmodifiableList(aulas);
-    }
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
 
-    public void adiciona(Aula aula) {
-        this.aulas.add(aula);
-    }
-    
-    public int getTempoTotal() {
-        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
-    }
-    
-    public String toString() {
-        return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal()
-                + ", aulas: + " + this.aulas + "]";
-    }
-    
-    public void matricula(Aluno aluno){
-        this.alunos.add(aluno);
-    }
-    
-    public Set<Aluno> getAlunos() {
-        return Collections.unmodifiableSet(alunos);
-    }
-    
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	public String toString() {
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: + " + this.aulas
+				+ "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		// adiciona no Set de alunos
+		this.alunos.add(aluno);
+		// cria a relação no Map
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
+	}
+	
+	public Aluno buscaMatriculado(int numero) {
+	    return this.matriculaParaAluno.get(numero);
+	}
+
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
+	public boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((alunos == null) ? 0 : alunos.hashCode());
+		result = prime * result + ((aulas == null) ? 0 : aulas.hashCode());
+		result = prime * result + ((instrutor == null) ? 0 : instrutor.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (alunos == null) {
+			if (other.alunos != null)
+				return false;
+		} else if (!alunos.equals(other.alunos))
+			return false;
+		if (aulas == null) {
+			if (other.aulas != null)
+				return false;
+		} else if (!aulas.equals(other.aulas))
+			return false;
+		if (instrutor == null) {
+			if (other.instrutor != null)
+				return false;
+		} else if (!instrutor.equals(other.instrutor))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
 }
